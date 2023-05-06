c75dd721a4dd3be9ef860c7f74c27b4ddf5d89a5
Ивайло Вълков <92274143+Nokraker@users.noreply.github.com>
2023-05-06 15:37:16 +0300

    Add files via upload

---
+++ 5-Sets and Dectionaries Advanced/Average Student Grades/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradeCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> grades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < gradeCount; i++)
+            {
+                string[] commands = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (grades.ContainsKey(commands[0]))
+                {
+                    grades[commands[0]].Add(decimal.Parse(commands[1]));
+                }
+                else
+                {
+                    grades.Add(commands[0], new List<decimal>()
+                    {
+                        decimal.Parse(commands[1])
+                    });
+                }
+            }
+
+            foreach (var kvp in grades)
+            {
+                string gradeOfStudent = string.Join(" ", kvp.Value.Select(x => x.ToString("F2")));
+                Console.WriteLine($"{kvp.Key} -> {gradeOfStudent} (avg: {kvp.Value.Average():f2})");
+            }
+        }
+    }
+}
---
+++ 5-Sets and Dectionaries Advanced/Average Student Grades/README.md
@@ -0,0 +1,4 @@
+# namå Average Student Grades
+# Óñëîâèå
+Create a program, which reads a name of a student and his/her grades and adds them to the student record, then
+prints the students' names with their grades and their average grade.
